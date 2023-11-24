3e617872338618d2d8a16043a45e668fe9e65aed
BrianMwas <brnmwas@gmail.com>
2023-11-24 15:43:34 +0300

    cleanup

--- py/src/ai/vdb.py
+++ py/src/ai/vdb.py
@@ -137,7 +137,7 @@ class VectorDb:
             """You are an assistant for question-answering tasks. \
                 Use the following pieces of retrieved context to answer the question. \
                 If you don't know the answer, just say that you don't know. \
-                Keep the answer concise.\
+                Keep the answer concise.
                 {context}
                 Question: {question}
             """
